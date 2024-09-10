b9c97f2a1344977b6a4ae4cd9f98c61debb33a31
Nhan Pham <dantedbartel@gmail.com>
2024-09-10 11:19:36 +0700

    Change route for ParamsHandler

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -21,14 +21,14 @@ func (a *Server) SetupRoutes(r *chi.Mux) {
 	r.Get("/v1/staker/delegation/check", registerHandler(handlers.CheckStakerDelegationExist))
 	r.Get("/v1/delegation", registerHandler(handlers.GetDelegationByTxHash))
 
-	registerCovenantHandler(r, handlers)
+	registerParamsHandler(r, handlers)
 	registerDAppHandler(r, handlers)
 	registerGmpHandler(r, handlers)
 
 	r.Get("/swagger/*", httpSwagger.WrapHandler)
 }
-func registerCovenantHandler(r *chi.Mux, handlers *handlers.Handler) {
-	r.Get("/v1/covenant/params", registerHandler(handlers.GetCovenantParams))
+func registerParamsHandler(r *chi.Mux, handlers *handlers.Handler) {
+	r.Get("/v1/params/covenant", registerHandler(handlers.GetCovenantParams))
 }
 func registerDAppHandler(r *chi.Mux, handlers *handlers.Handler) {
 	r.Get("/v1/dApp", registerHandler(handlers.GetDApp))
